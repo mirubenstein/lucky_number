0edd5f7193a002fd996fc0ff47aded2ad7e6e5e8
mir <mightykiwi@yahoo.com>
2014-07-31 22:35:04 -0700

    Add functionality and test for half lucky numbers

--- lib/luck_check.rb
+++ lib/luck_check.rb
@@ -1,4 +1,17 @@
 def luck_check(number)
+
   numbers = number.to_s.split('').map {|i| i.to_i}
-  numbers.slice(0..(numbers.length / 2 -1)).reduce(:+) == numbers.slice((numbers.length / 2.0).ceil..-1).reduce(:+) ? true : false
+  left_sum = numbers.slice(0..(numbers.length / 2 -1)).reduce(:+)
+  right_sum = numbers.slice((numbers.length / 2.0).ceil..-1).reduce(:+)
+  left_sum_sum = left_sum.to_s.split('').map {|i| i.to_i}
+  right_sum_sum = right_sum.to_s.split('').map {|i| i.to_i}
+
+  if left_sum == right_sum
+    return "Lucky Number"
+  elsif left_sum == right_sum_sum.reduce(:+) || right_sum == left_sum_sum.reduce(:+)
+    return "Half Lucky Number"
+  else
+    return "No Lucky Number"
+  end
+
 end
--- spec/luck_check_spec.rb
+++ spec/luck_check_spec.rb
@@ -4,19 +4,27 @@ require 'luck_check'
 describe 'luck_check' do
 
   it 'checks if left side of number is equal to right side of number for even length numbers' do
-    expect(luck_check(123321)).to eq true
+    expect(luck_check(123321)).to eq "Lucky Number"
   end
 
   it 'checks if left side of number is equal to right side of number for odd length numbers' do
-    expect(luck_check(1236321)).to eq true
+    expect(luck_check(1236321)).to eq "Lucky Number"
   end
 
   it 'checks if left side of number is not equal to right side of number for even length numbers' do
-    expect(luck_check(666777)).to eq false
+    expect(luck_check(666777)).to eq "No Lucky Number"
   end
 
   it 'checks if left side of number is not equal to right side of number for odd length numbers' do
-    expect(luck_check(6666777)).to eq false
+    expect(luck_check(6666777)).to eq "No Lucky Number"
+  end
+
+  it 'checks if either side sum of number is equal to opposite side of number added for even length numbers' do
+    expect(luck_check(131761)).to eq "Half Lucky Number"
+  end
+
+  it 'checks if either side sum of number is equal to opposite side of number added for odd length numbers' do
+    expect(luck_check(131761)).to eq "Half Lucky Number"
   end
 
 end

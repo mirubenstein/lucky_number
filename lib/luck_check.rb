13bb7109b0752bec79ecc79f3b6481ebe22ab65f
mir <mightykiwi@yahoo.com>
2014-07-31 22:08:26 -0700

    Refactor functionality for comparing left and right string sums

--- lib/luck_check.rb
+++ lib/luck_check.rb
@@ -1,21 +1,4 @@
 def luck_check(number)
-  numbers = number.to_s.split''
-  left_half = numbers.slice(0..(numbers.length / 2.0 -1).floor)
-  right_half = numbers.slice((numbers.length / 2.0).ceil..-1)
-  left_half_sum = 0
-  right_half_sum = 0
-
-  left_half.each do |num|
-    left_half_sum += num.to_i
-  end
-
-  right_half.each do |num|
-    right_half_sum += num.to_i
-  end
-
-  if left_half_sum == right_half_sum
-    return true
-  else
-    return false
-  end
+  numbers = number.to_s.split('').map {|i| i.to_i}
+  numbers.slice(0..(numbers.length / 2.0 -1).floor).reduce(:+) == numbers.slice((numbers.length / 2.0).ceil..-1).reduce(:+) ? true : false
 end

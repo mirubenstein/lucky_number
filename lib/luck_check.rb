e66babe9a9ee91d76051772df0535e3a29665656
mir <mightykiwi@yahoo.com>
2014-07-31 22:17:50 -0700

    add full coverage for tests for edge cases

--- lib/luck_check.rb
+++ lib/luck_check.rb
@@ -1,4 +1,4 @@
 def luck_check(number)
   numbers = number.to_s.split('').map {|i| i.to_i}
-  numbers.slice(0..(numbers.length / 2.0 -1).floor).reduce(:+) == numbers.slice((numbers.length / 2.0).ceil..-1).reduce(:+) ? true : false
+  numbers.slice(0..(numbers.length / 2 -1)).reduce(:+) == numbers.slice((numbers.length / 2.0).ceil..-1).reduce(:+) ? true : false
 end
--- spec/luck_check_spec.rb
+++ spec/luck_check_spec.rb
@@ -2,7 +2,21 @@ require 'rspec'
 require 'luck_check'
 
 describe 'luck_check' do
-  it 'checks if left side of number is equal to right side of number' do
+
+  it 'checks if left side of number is equal to right side of number for even length numbers' do
+    expect(luck_check(123321)).to eq true
+  end
+
+  it 'checks if left side of number is equal to right side of number for odd length numbers' do
     expect(luck_check(1236321)).to eq true
   end
+
+  it 'checks if left side of number is not equal to right side of number for even length numbers' do
+    expect(luck_check(666777)).to eq false
+  end
+
+  it 'checks if left side of number is not equal to right side of number for odd length numbers' do
+    expect(luck_check(6666777)).to eq false
+  end
+
 end
